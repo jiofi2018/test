884560175f009941d341789b06af9771588b4f5a
jiofi2018 <63831048+jiofi2018@users.noreply.github.com>
2022-11-19 08:13:47 +0530

    initial port commit

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express()
 app.get("/",(req,res)=>{
     res.send("working by me")
 });
-app.listen(4000,()=>{
-    console.log("Server working....");
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
